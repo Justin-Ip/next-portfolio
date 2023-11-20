121eeb5bcce4faa74e5b801743e0f5d6f9965df5
Justin Ip <Justinip44@gmail.com>
2023-11-20 16:15:39 -0500

    rewrite description

--- app/page.js
+++ app/page.js
@@ -41,7 +41,7 @@ export default function Home() {
                   About Me
                 </Typography>
                 <Typography variant="p" paragraph={true}>
-                  Hello, I&apos;m Justin Ip, an engineering and additive manufacturing professional with a strong background in prototyping and product design. I&apos;m proficient in 3D modeling using CAD software like SolidWorks, Creo ProE, Blender, and Rhino.
+                  I&apos;m an engineering and additive manufacturing professional with a strong background in prototyping and product design. I&apos;m proficient in 3D modeling using CAD software like SolidWorks, Creo ProE, Blender, and Rhino.
                 </Typography>
               </Grid>
             </Grid>
